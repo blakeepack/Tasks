09040084bd48efc8b3d2c01bd7434235a9deb91c
Blake Pack <blakeepack@yahoo.com>
2020-01-22 15:05:52 -0500

    blakeepack Tasks 02 upload b

--- Task_02/Task_02b (Autosaved).R
+++ Task_02/Task_02b (Autosaved).R
@@ -22,25 +22,35 @@ head(Data)
 beren2 <- Data
 beren3 <- beren2[order(beren2$age),]
 write.csv(beren3, "beren_new.csv", quote=F, row.names=FALSE)
+
+Feeds <- which(beren3$event == "bottle")
+
 avgMilk <- mean(beren3$value[Feeds])
+# fluid oz
+#because it contains the numerical measurement
+#It calls back to the previously set object that we set for the event bottle
 avgFeed <- tapply(beren3$value [Feeds], beren3$age[Feeds], mean, na.rm=T)
 varFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], var, na.rm=T)
 totalFeed <- tapply(beren3$value[Feeds], beren3$age[Feeds], sum, na.rm=T)
-numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length, na.rm=T)
+numFeeds <- tapply(beren3$value[Feeds], beren3$age[Feeds], length)
 cor(beren3$value[Feeds], beren3$age[Feeds])
 cor.test(beren3$value[Feeds], beren3$age[Feeds])
 berenCor <- cor.test(beren3$value[Feeds], beren3$age[Feeds])
 summary(berenCor)
 berenANOVA <- aov(beren3$value[Feeds] ~ beren3$caregiver[Feeds])
 boxplot( beren3$value[Feeds] ~ beren3$caregiver[Feeds], xlab= "who gave the bottle", ylab= "amount of milk consumed (oz)")
-?par
 par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck=-0.01)
 plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
 abline(h= avgFeed, lty=2, col='red')
 
 pdf("r02b-totalMilkByDay.pdf", height=4, width=4)
+
 par(las=1, mar=c(5, 5, 1, 1), mgp=c(2, 0.5, 0), tck=-0.01)
 plot(as.numeric(names(totalFeed)), totalFeed, type="b", pch=16, xlab="age in days", ylab="ounces of milk")
-#abline(h=avgFeed, lty=2, col='red')
+abline(h=mean(totalFeed), lty=2, col='red')
+
 dev.off()
+
+source("http://jonsmitchell.com/code/plotFxn02b.R")
+
 
