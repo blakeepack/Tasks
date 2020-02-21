cdf1a48cfa97eb14358b269a2f861f2584d7e134
Blake Pack <blakeepack@yahoo.com>
2020-02-21 10:24:42 -0500

    blakeepack Task 06 upload

---
+++ Evolution Paper Work/Hypothesis Data Work.R
@@ -0,0 +1,4 @@
+setwd("~/Desktop/Evolution/Evolution Paper Work")
+Data <- read.csv(http://esapubs.org/archive/ecol/E090/184/PanTHERIA_1-0_WR93_Aug2008.txt)
+Data <- read.csv("http://esapubs.org/archive/ecol/E090/184/PanTHERIA_1-0_WR93_Aug2008.txt")
+read.table(
